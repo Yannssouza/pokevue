74dee12df4ef8f30f069165d38dca3ff86003727
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-02-10 11:17:45 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+
+let num = ref(0);
+</script>
+
+<template>
+  <div>
+    <button @click="num++">
+      {{ num }}
+    </button>
+    <h1 class="m-10 text-3xl font-bold text-red-600">Hello World</h1>
+  </div>
+</template>
