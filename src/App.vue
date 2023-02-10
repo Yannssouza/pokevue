93d013d7a5ee981075bf923b748f551f7f7b44fc
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-02-10 12:57:01 -0300

    router fix

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup>
-import { ref } from "vue";
-
-let num = ref(0);
 </script>
 
 <template>
   <div class="flex h-full w-full flex-col">
     <nav>
-      <div class="flex h-12 items-center justify-center gap-8 bg-blue-500 p-5 text-white font-bold">
-        <router-link to="/home">Home</router-link>
-        <router-link to="/about">About</router-link>
+      <div
+        class="flex h-12 items-center justify-center gap-8 bg-blue-500 p-5 font-bold text-white"
+      >
+        <router-link to="/">Inicio</router-link>
+        <router-link to="/About">Sobre</router-link>
       </div>
     </nav>
 
@@ -18,7 +17,7 @@ let num = ref(0);
     </main>
 
     <footer
-      class="fixed bottom-0 flex h-12 w-full items-center justify-center bg-blue-500 text-white font-bold"
+      class="fixed bottom-0 flex h-12 w-full items-center justify-center bg-blue-500 font-bold text-white"
     >
       <span class="">{{ new Date().getFullYear() }}</span>
     </footer>
