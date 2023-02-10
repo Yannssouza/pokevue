4e5f9cfab8c7d13f3125199b4d01419df2b7f02a
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-02-10 14:00:23 -0300

    card

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
 </script>
 
 <template>
-  <div class="flex h-full w-full flex-col">
+  <div>
     <nav>
       <div
-        class="flex h-12 items-center justify-center gap-8 bg-blue-500 p-5 font-bold text-white"
+        class="flex h-12 items-center justify-center xl:justify-start gap-12 px-8 bg-blue-500 p-5 font-bold text-white"
       >
         <router-link to="/">Inicio</router-link>
         <router-link to="/About">Sobre</router-link>
@@ -17,7 +17,7 @@
     </main>
 
     <footer
-      class="fixed bottom-0 flex h-12 w-full items-center justify-center bg-blue-500 font-bold text-white"
+      class="fixed bottom-0 w-full flex h-12 items-center justify-center bg-blue-500 font-bold text-white"
     >
       <span class="">{{ new Date().getFullYear() }}</span>
     </footer>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-    <h1>Hello World!</h1>
+  <div class="container mx-auto mt-48 xl:mt-12 flex justify-center">
+    <div class="flex justify-center">
+      <div class="max-w-sm rounded-l-lg bg-white border-l border-t border-b">
+        <a>
+          <img
+            class="rounded-lg"
+            src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+            alt=""
+          />
+        </a>
+        <div class="p-6">
+          <h5 class="mb-2 text-xl font-medium text-gray-900">Card title</h5>
+          <p class="mb-4 text-base text-gray-700">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="flex justify-center">
+      <div class="max-w-sm rounded-r-lg bg-white border-r border-t border-b">
+        <div class="p-6">
+          <h5 class="mb-2 text-xl font-medium text-gray-900">Card title</h5>
+          <p class="mb-4 text-base text-gray-700">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
