bf19fcde353e5984895997cc4b471a298178c40c
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-02-11 13:35:33 -0300

    readme fix

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -13,7 +13,6 @@ const pokemon = defineProps([
   "type",
   "height",
   "weight",
-
 ]);
 </script>
 
@@ -51,19 +50,19 @@ const pokemon = defineProps([
         </div>
         <div class="mb-2 flex flex-row items-center justify-center">
           <h2 class="pr-2 font-medium text-gray-900">special defense:</h2>
-          <p class="text-base text-gray-700">{{pokemon.sDefense}}</p>
+          <p class="text-base text-gray-700">{{ pokemon.sDefense }}</p>
         </div>
         <div class="mb-2 flex flex-row items-center justify-center">
           <h2 class="pr-2 font-medium text-gray-900">speed:</h2>
-          <p class="text-base text-gray-700">{{pokemon.speed}}</p>
+          <p class="text-base text-gray-700">{{ pokemon.speed }}</p>
         </div>
         <div class="mb-2 flex flex-row items-center justify-center">
           <h2 class="pr-2 font-medium text-gray-900">type:</h2>
-          <p class="text-base text-gray-700">{{pokemon.type}}</p>
+          <p class="text-base text-gray-700">{{ pokemon.type }}</p>
         </div>
         <div class="mb-2 flex flex-row items-center justify-center">
           <h2 class="pr-2 font-medium text-gray-900">base xp:</h2>
-          <p class="text-base text-gray-700">{{pokemon.xp}}</p>
+          <p class="text-base text-gray-700">{{ pokemon.xp }}</p>
         </div>
       </div>
     </div>
